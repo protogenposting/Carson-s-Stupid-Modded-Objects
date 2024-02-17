cbd7dc64bd0f4576537a17b8f9deaea3cdd82e18
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-02-17 15:23:06 -0500

    fghfhfgh

---
+++ Loenn/entities/ReverseBumper.lua
@@ -0,0 +1,9 @@
+local ReverseBumper = {}
+
+ReverseBumper.name = "Carson_s_Stupid_Modded_Objects/ReverseBumper"
+ReverseBumper.texture = "objects/BumperIdle/22"
+ReverseBumper.placements = {
+    name = "Reverse Bumper",
+	data = {}
+}
+return ReverseBumper
--- Objects/Entities/ReverseBumper.cs
+++ Objects/Entities/ReverseBumper.cs
@@ -8,10 +8,8 @@ namespace Celeste.Mod.Carson_s_Stupid_Modded_Objects.Objects.Entities
 {
     [CustomEntity("Carson_s_Stupid_Modded_Objects/ReverseBumper")]
     public class ReverseBumper : Bumper{
-        private readonly DynamicData BaseData;
         public ReverseBumper(EntityData data, Vector2 offset) : base(data, offset)
         {
-            BaseData = DynamicData.For(this);
             PlayerCollider pc = Get<PlayerCollider>();
             var orig = pc.OnCollide;
             pc.OnCollide = player => {
